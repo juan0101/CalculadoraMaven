199300a47a51999396dac4d055dac4b321c6a78c
Juan Vinicius <juan_vinicius_19@yahoo.com.br>
2016-03-14 09:51:44 -0300

    Projeto calculadora
    projeto calculadora maven

---
+++ Calculadora/src/main/java/br/com/calc/hu3tech/MainCalc.java
@@ -0,0 +1,14 @@
+package br.com.calc.hu3tech;
+
+import java.util.Scanner;
+
+public class MainCalc {
+
+	private static Scanner scanner;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
